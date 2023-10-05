da4b6cd48042bb16cd0e517fbf40a26cb6825fc7
Thiago <lucimara1422@gmail.com>
2023-10-04 22:08:01 -0300

    feat(const) header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Thiago Mota';
+export const HEADER_TITLE = '<Thiago />'
 export const SITE_DESCRIPTION = 'Welcome to my website!';
